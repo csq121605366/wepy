3de63304850cc58db57ac9c3b72138f33ebee918
caishangqing <121605366@qq.com>
2018-04-10 19:05:57 +0800

    546

--- src/vendor/index.js
+++ src/vendor/index.js
@@ -1,5 +1,22 @@
 // import _ from 'lodash';
 // global._ = _;
+import wepy from 'wepy'
+import * as qiniu from 'qiniu-js/dist/qiniu.min.js'
 
-import * as qiniu from 'qiniu-js/dist/qiniu.min.js';
-global.qiniu = qiniu;
+wepy.qiniu = qiniu
+
+const asyncWrap = fn => (options = {}) => new Promise((resolve, reject) => {
+  let conf = {
+    success: res => {
+      resolve(res)
+    },
+    fail: err => {
+      reject(err)
+    }
+  }
+  wepy[fn](Object.assign({}, conf, options))
+})
+
+wepy.requestAsync = asyncWrap('request')
+wepy.loginAsync = asyncWrap('login')
+wepy.getSystemInfoAsync = asyncWrap('getSystemInfo')
